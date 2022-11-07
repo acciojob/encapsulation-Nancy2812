cf88fa306e72e3c6dd082418a9d6ba50e6fb8e39
Nan2812 <nancysinha1998@gmail.com>
2022-11-07 13:02:20 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[])
+  {
+      RWOnly obj=new RWOnly();
+     // String ans=obj.name("Hello");
+      //name' has private access in 'com.driver.RWOnly'
+      obj.setName("Nancy");
+      String ans=obj.getName();
+      System.out.print(ans);
+  }
 }
